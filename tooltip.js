c24c5820f522205f519a2a601bb02a2dce6ea191
Alex <olexandrmai@gmail.com>
2021-08-31 13:29:47 +0300

    add some changes

--- tooltip.js
+++ tooltip.js
@@ -14,9 +14,30 @@ class Tooltip extends HTMLElement {
                 position: absolute;
                 z-index: 10; 
             }
+
+            :host {
+                background: #ccc;
+            }
+
+            .highlight {
+                background-color: red;
+            }
+
+            ::slotted(.highlight) {
+                border-bottom: 1px dotted red;
+                // border-bottom: 2px solid blue;
+            }
+
+            .icon {
+                background: black;
+                color: white;
+                padding: 0.15rem 0.5rem;
+                text-align: center;
+                border-radius: 50%;
+            }
         </style>
         <slot>Some defolt</slot>
-        <span> (?)</span>
+        <span class="icon">?</span>
         `;
     }
 
